74984f3a62b19cd9df060423ec6233088c8a42fc
GabriPires <gabriel.santos.pes@gmail.com>
2020-06-04 11:47:17 -0300

    Criando página Detail

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
+import Constants from 'expo-constants';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Detail = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
+  return (
+   <SafeAreaView style={{ flex: 1 }}>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handleNavigateBack}>
+        <Icon name="arrow-left" size={20} color="#34cb79"/>
+      </TouchableOpacity>
+
+      <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1506484381205-f7945653044d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }}/>
+
+      <Text style={styles.pointName}>Mercadão do Zé</Text>
+      <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+
+      <View style={styles.address}>
+        <Text style={styles.addressTitle}>Endereço</Text>
+        <Text style={styles.addressContent}>Guaratinguetá, SP</Text>
+      </View>
+    </View>
+
+    <View style={styles.footer}>
+      <RectButton style={styles.button} onPress={() => {}}>
+        <FontAwesome name="whatsapp" size={20} color="#FFF"/>
+        <Text style={styles.buttonText}>Whatsapp</Text>
+      </RectButton>
+      <RectButton style={styles.button} onPress={() => {}}>
+        <Icon name="mail" size={20} color="#FFF"/>
+        <Text style={styles.buttonText}>Email</Text>
+      </RectButton>
+    </View>
+   </SafeAreaView>
+  );
 }
 
 export default Detail;
@@ -11,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    paddingTop: 20,
+    paddingTop: 20 + Constants.statusBarHeight,
   },
 
   pointImage: {
